fe829e4bf2497f09979a72629dbd9c01dec9870a
Thomas Alvenin <105067427+ThomasAlvenin@users.noreply.github.com>
2023-01-28 16:14:55 +0100

    Actualización 1.0.1 - Mejoras en la visualización de las IDs.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,11 @@
-client_script 'client.lua'
+fx_version 'adamant'
+games { 'gta5' }
+
+author 'Thomas Alvenin'
+version '1.0.1'
+
+description 'Script básico para enseñar IDs por tecla. Permite mostrar la ID de un jugador al presionar una tecla específica.'
+
+client_scripts {
+    'client.lua'
+}
